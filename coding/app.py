515ab3a3034d306be0ba179d1012ab52a572394c
boedoet52 <zackiferdinansyah144@gmail.com>
2025-10-30 11:31:08 +0700

    new file with name app.py

---
+++ coding/app.py
@@ -0,0 +1,14 @@
+print('Selamat Datang')
+
+nilai = int(input('Masukkan Nilai Akhir :'))
+
+if nilai >= 90:
+    print('A')
+elif nilai >= 80:
+    print('B')
+elif nilai >= 70:
+    print('C')
+elif nilai >= 60:
+    print('D')
+else:
+    print('E')
